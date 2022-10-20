c7b32cf2a7e8e2ae16b9193206b1f04509b98a92
alexqdavid <alexqdavid@icloud.com>
2022-10-20 14:44:10 -0400

    rollbar

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.get('/', getHTML)
 app.get('/css', getCSS)
 app.get('/js', getJS)
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'db8f7e8361a04cf190d6446409592492',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const port = process.env.PORT || 4000
 
 app.listen(port, console.log(`Server running on ${port}`))
