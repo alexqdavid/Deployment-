f804864d820efb4ceff120049de3bc9c80c54fa9
alexqdavid <alexqdavid@icloud.com>
2022-10-20 16:48:12 -0400

    rollbar

--- public/main.js
+++ public/main.js
@@ -7,11 +7,3 @@ const helloAlert = evt => {
 
 llama.addEventListener('click', helloAlert)
 
-let playAudio = document.querySelector('audio')
-
-const audioAlert = evt => {
-
-}
-
-playAudio.addEventListener('click', audioAlert)
-
