4e0f72906d69f2a63687f85d69166beec9141db0
alexqdavid <alexqdavid@icloud.com>
2022-10-20 15:43:37 -0400

    rollbar

--- public/main.js
+++ public/main.js
@@ -3,14 +3,8 @@ const Rollbar = require("rollbar")
 let llama = document.querySelector('img')
 
 const helloAlert = evt => {
-    Rollbar.log('wanna see the muffin man?');
     alert('do you know the muffin man?')
 }
 
 llama.addEventListener('click', helloAlert)
 
-let x = document.getElementById("myaudio");
-
-function playAudio() {
-    x.play();
-}
