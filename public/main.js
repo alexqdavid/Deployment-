f75cfbd1dc9bdfb7e80b49cf66696d679058cf82
alexqdavid <alexqdavid@icloud.com>
2022-10-26 22:24:51 -0400

    html update

--- public/main.js
+++ public/main.js
@@ -1,18 +1,4 @@
 
-let llama = document.querySelector('img')
 
-const helloAlert = evt => {
-    alert('do you know the muffin man?')
-}
-
-llama.addEventListener('click', helloAlert)
-
-let song = document.querySelector('audio')
-
-function playAudio() {
-    song.play();
-}
-
-song.addEventListener('click',playAudio)
 
 
